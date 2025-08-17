220dffb7afc47807041eb3ec431aab6cbbefeb5b
Mohammed <smaaz643@gmail.com>
2025-08-17 19:37:55 +0100

    add blob packag

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   // uncomment next line if the error is TypeScript-only and you need to ship now:
  typescript: { ignoreBuildErrors: true },
 
-  images: { domains: ["lh3.googleusercontent.com"] },
+  images: { domains: ["lh3.googleusercontent.com", "public.blob.vercel-storage.com"] },
 };
 
 export default nextConfig;
